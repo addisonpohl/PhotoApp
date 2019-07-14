c848ac208c1a78ad20dafeb1a8ddf384c6e95357
addisonpohl <addison.c.pohl@gmail.com>
2019-07-14 01:23:15 -0500

    Add update controller and route

--- photo_app/app/controllers/api/photos_controller.rb
+++ photo_app/app/controllers/api/photos_controller.rb
@@ -19,11 +19,19 @@ class Api::PhotosController < ApplicationController
     render "show.json.jbuilder"
   end
 
+  def update
+    @photo = Photo.find_by(id: params[:id])
+    @photo.name = params[:name] || @photo.name
+    @photo.height = params[:height] || @photo.height
+    @photo.width = params[:width] || @photo.width
+    @photo.save
+    render "show.json.jbuilder"
+  end
+
   def delete
     @photo = Photo.find_by(id: params[:id])
     name = @photo.name
     @photo.destroy
     render json: {message: "Photo: #{name} Deleted"}
   end
-
 end
--- photo_app/config/routes.rb
+++ photo_app/config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     get "/photos" => "photos#index"
     get "/photos/:id" => "photos#show"
     post "/photos" => "photos#create"
+    patch "/photos/:id" => "photos#update"
     delete "/photos/:id" => "photos#delete"
+
   end
 end
